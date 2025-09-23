872cc15e9c5c93ac1251a9dccac214432cf17ad3
Aramide-jk <aramjay29@gmail.com>
2025-09-23 08:23:20 +0100

    Vit.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,28 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "process.env.VITE_BACKEND_URL|| http://localhost:5000",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target:
-          "jkautos-backend-production.up.railway.app || http://localhost:5000",
-        // target: "http://localhost:5000",
+        target: process.env.VITE_BACKEND_URL || "http://localhost:5000",
         changeOrigin: true,
       },
     },
